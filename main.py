e563ee4ea9da9a721232c25d01aabb64a599f52d
Shade <55607744+schmeow@users.noreply.github.com>
2024-04-08 09:03:17 -0400

    one channel per user !!

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from discord import Intents, Client, app_commands, Object, Interaction
 from dotenv import load_dotenv
 
 import csv
-import pandas as pd
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
@@ -18,26 +17,28 @@ tree = app_commands.CommandTree(client)
 
 @tree.command(
     name="create_channel",
-    description="My first application Command",
+    description="Create your youtube channel",
     guild=Object(id=1224912313023467531)
 )
 async def first_command(interaction: Interaction, channel_name: str):
-
-    df = pd.read_csv('user_data.csv')
-    ids = df.user_id
-
-    if interaction.user.id in ids:
-        return
+    with open('user_data.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if row[0] == str(interaction.user.id):
+                await interaction.response.send_message("You have already created a channel!")
+                return
 
     with open('user_data.csv', 'a') as file:
         writer = csv.writer(file)
         writer.writerow([interaction.user.id, channel_name])
+        await interaction.response.send_message("You have created a new channel!")
 
 
 @client.event
 async def on_ready() -> None:
     await tree.sync(guild=Object(id=1224912313023467531))
     print(f'{client.user} is now running!')
+
 
 
 def main() -> None:
